a0c5aeb880b82d60b4d63964234fd5335f26511a
VictorinoBF97 <vito.blazquez.97@outlook.com>
2019-05-24 12:04:08 +0200

    Commit final con la funcionalidad de la subida de archivos

--- resources/views/public/userfiles/index.blade.php
+++ resources/views/public/userfiles/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'File's user MEGAUPLOAD')
+@section('title', 'Files user MEGAUPLOAD')
 <!-- Vista de la lista de archivos subidos por un usuario -->
 @section('content')
 
